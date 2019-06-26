b5a6ddc6b23a6d6610100fe2845e6cdf58aad3c1
pedro_bras <pedro.c.bras@gmail.com>
2019-06-26 20:16:00 +0200

    adds state to app class with dummy todos

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
+import Todos from './components/Todos';
 
-function App() {
+class App extends Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: 'Take out the trash',
+        completed: false,
+      },
+      {
+        id: 2,
+        title: 'Dinner with wife',
+        completed: false,
+      },
+      {
+        id: 3,
+        title: 'Meeting with boss',
+        completed: false,
+      },
+    ]
+  }
+  render() {
   return (
     <div className="App">
-      <h1>App</h1>
+      <Todos />
     </div>
   );
 }
-
+}
 export default App;
